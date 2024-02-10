c89351d304e74382f3fd78283a34a76a98095424
RyanAtRowan <astralpeaches@gmail.com>
2024-02-10 13:27:06 -0500

    Added admin and customer areas (corrected necessary routes), early work on navbar dropdown started.

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
@@ -29,8 +29,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Default map controller does need some work for this design pattern!
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
