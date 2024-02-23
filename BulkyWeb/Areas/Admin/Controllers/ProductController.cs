d0a1577599b221f138d12c50af84c375636c9b57
RyanAtRowan <astralpeaches@gmail.com>
2024-02-23 14:05:53 -0500

    Fully integrated Datatables.net tool. using sweetalert2 to allow confirmation of delete.

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -119,6 +119,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return View();
         }
 
+        /**
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
@@ -149,6 +150,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             TempData["success"] = "Product deleted successfully";
             return RedirectToAction("Index", "Product");
         }
+        **/
 
         #region API CALLS
 
@@ -157,6 +159,30 @@ namespace BulkyWeb.Areas.Admin.Controllers
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return Json(new {data = objProductList});
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if(productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting"});
+            }
+
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath,
+                productToBeDeleted.ImageUrl.TrimStart('\\'));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
         }
 
         #endregion
